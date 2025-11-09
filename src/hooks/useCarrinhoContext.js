784a95c05626a7431fe74a10cc063adc9e21cc2f
neosamdez <leonardoaguiar080@gmail.com>
2025-11-09 14:20:09 -0300

    remover menu supenso

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -61,9 +61,16 @@ export const useCarrinhoContext = () => {
     }  
   }
 
+    function removerProdutoCarrinho(id) {
+      const produto = carrinho.filter(
+        (itemDoCarrinho) => itemDoCarrinho.id !== id);
+      setCarrinho(produto);  
+    }
+
     return { 
         carrinho,setCarrinho, 
         adicionarProduto, 
-        removerProduto  
+        removerProduto,
+        removerProdutoCarrinho  
     };
 };
